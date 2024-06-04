b28a5f9bd51c473ed5d16422f0c3822533200261
suryalimbu <suryamankedem@gmail.com>
2024-06-04 23:52:07 +0100

    bug fix

--- app/flightBooking/bookingConfirmation/page.tsx
+++ app/flightBooking/bookingConfirmation/page.tsx
@@ -72,7 +72,11 @@ export default function Page() {
       setBookingData(data);
     };
     fetchAPI();
-  }, []);
+  }, [userId]);
+
+  if (!bookingData.length) {
+    return <div>Loading...</div>;
+  }
 
   const totalPassengers = bookingData[0].passengers.length;
 
@@ -99,116 +103,96 @@ export default function Page() {
             </span>
           </div>
           {bookingData.map((booking) => (
-            <div
-              key={booking._id}
-              className="space-y-4 p-4 border rounded-b-xl"
-            >
+            <div key={booking._id} className="space-y-4 p-4 border rounded-b-xl">
               <div>
                 <h1>Boarding Pass</h1>
                 <h1>Booking Id: {booking._id}</h1>
               </div>
               <div className="space-y-4">
-                {booking.flightId &&
-                  booking.flightId.departureAirport &&
-                  booking.flightId.arrivalAirport && (
-                    <>
-                      <div className="flex justify-between items-center">
-                        <div>
-                          <span>
-                            {booking.flightId.departureAirport.location}
-                          </span>
-                          <br />
-                          <span className="font-bold text-lg sm:text-2xl">
-                            {booking.flightId.departureAirport.airportCode}
-                          </span>
-                          <br />
-                          <span className="text-4xl">
-                            <PiAirplaneTakeoff />
-                          </span>
-                        </div>
-                        <div className="flex items-center">
-                          <span className="text-3xl mx-4">----</span>
-                          <span>
-                            <PiAirplane className="rotate-90 text-3xl" />
-                          </span>
-                          <span className="text-3xl mx-4">----</span>
-                        </div>
-                        <div>
-                          <span>
-                            {booking.flightId.arrivalAirport.location}
-                          </span>
-                          <br />
-                          <span className="font-bold text-lg sm:text-2xl">
-                            {booking.flightId.arrivalAirport.airportCode}
-                          </span>
-                          <br />
-                          <span className="text-4xl">
-                            <PiAirplaneLanding />
-                          </span>
-                        </div>
+                {booking.flightId && booking.flightId.departureAirport && booking.flightId.arrivalAirport && (
+                  <>
+                    <div className="flex justify-between items-center">
+                      <div>
+                        <span>{booking.flightId.departureAirport.location}</span>
+                        <br />
+                        <span className="font-bold text-lg sm:text-2xl">
+                          {booking.flightId.departureAirport.airportCode}
+                        </span>
+                        <br />
+                        <span className="text-4xl">
+                          <PiAirplaneTakeoff />
+                        </span>
                       </div>
-                      <hr />
-                      <div className="flex justify-between">
-                        <div>
-                          <span>Departure</span>
-                          <br />
-                          <span className="font-bold text-lg sm:text-2xl">
-                            {new Date(
-                              booking.flightId.departureTime
-                            ).toLocaleTimeString()}
-                          </span>
-                        </div>
-                        <div>
-                          <span>Departure</span>
-                          <br />
-                          <span className="font-bold text-lg sm:text-2xl">
-                            {calculateDuration(
-                              booking.flightId.departureTime,
-                              booking.flightId.arrivalTime
-                            )}
-                          </span>
-                        </div>
-                        <div>
-                          <span>Arrival</span>
-                          <br />
-                          <span className="font-bold text-lg sm:text-2xl">
-                            {new Date(
-                              booking.flightId.arrivalTime
-                            ).toLocaleTimeString()}
-                          </span>
-                        </div>
+                      <div className="flex items-center">
+                        <span className="text-3xl mx-4">----</span>
+                        <span>
+                          <PiAirplane className="rotate-90 text-3xl" />
+                        </span>
+                        <span className="text-3xl mx-4">----</span>
                       </div>
-                      <hr />
-                      <div className="flex justify-between text-xl gap-4">
-                        <div className="p-4 rounded-lg">
-                          <h1>Seats</h1>
-                          <span className="font-bold text-lg sm:text-2xl text-primary">
-                            {booking.seatId
-                              .map((seat) => seat.seatName)
-                              .join(", ")}
-                          </span>
-                        </div>
-                        <div className="p-4 rounded-lg">
-                          <h1>Flight</h1>
-                          <span className="font-bold text-lg sm:text-2xl text-primary">
-                            {booking.flightId.flightNumber}
-                          </span>
-                        </div>
-                        <div className="p-4 rounded-lg">
-                          <h1>Class</h1>
-                          <span className="font-bold text-lg sm:text-2xl text-primary">
-                            Business
-                          </span>
-                        </div>
+                      <div>
+                        <span>{booking.flightId.arrivalAirport.location}</span>
+                        <br />
+                        <span className="font-bold text-lg sm:text-2xl">
+                          {booking.flightId.arrivalAirport.airportCode}
+                        </span>
+                        <br />
+                        <span className="text-4xl">
+                          <PiAirplaneLanding />
+                        </span>
                       </div>
-                    </>
-                  )}
+                    </div>
+                    <hr />
+                    <div className="flex justify-between">
+                      <div>
+                        <span>Departure</span>
+                        <br />
+                        <span className="font-bold text-lg sm:text-2xl">
+                          {new Date(booking.flightId.departureTime).toLocaleTimeString()}
+                        </span>
+                      </div>
+                      <div>
+                        <span>Duration</span>
+                        <br />
+                        <span className="font-bold text-lg sm:text-2xl">
+                          {calculateDuration(booking.flightId.departureTime, booking.flightId.arrivalTime)}
+                        </span>
+                      </div>
+                      <div>
+                        <span>Arrival</span>
+                        <br />
+                        <span className="font-bold text-lg sm:text-2xl">
+                          {new Date(booking.flightId.arrivalTime).toLocaleTimeString()}
+                        </span>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="flex justify-between text-xl gap-4">
+                      <div className="p-4 rounded-lg">
+                        <h1>Seats</h1>
+                        <span className="font-bold text-lg sm:text-2xl text-primary">
+                          {booking.seatId.map((seat) => seat.seatName).join(", ")}
+                        </span>
+                      </div>
+                      <div className="p-4 rounded-lg">
+                        <h1>Flight</h1>
+                        <span className="font-bold text-lg sm:text-2xl text-primary">
+                          {booking.flightId.flightNumber}
+                        </span>
+                      </div>
+                      <div className="p-4 rounded-lg">
+                        <h1>Class</h1>
+                        <span className="font-bold text-lg sm:text-2xl text-primary">
+                          Business
+                        </span>
+                      </div>
+                    </div>
+                  </>
+                )}
               </div>
               <hr />
               <div>
-                <h1 className="uppercase text-xl sm:3xl font-semibold">
-                  Passengers
-                </h1>
+                <h1 className="uppercase text-xl sm:3xl font-semibold">Passengers</h1>
                 <ul className="py-4 space-y-4">
                   {booking.passengers.map((passenger) => (
                     <li key={passenger._id} className="flex items-center gap-4">
@@ -219,46 +203,31 @@ export default function Page() {
                           alt="Passenger"
                         />
                       </span>
+                      <span className="text-lg sm:text-xl text-primary">{passenger.fullName}</span>
                       <span className="text-lg sm:text-xl text-primary">
-                        {passenger.fullName}
-                      </span>
-                      <span className="text-lg sm:text-xl text-primary">
-                        (
-                        {passenger.gender.charAt(0).toUpperCase() +
-                          passenger.gender.slice(1)}
-                        )
+                        ({passenger.gender.charAt(0).toUpperCase() + passenger.gender.slice(1)})
                       </span>
                     </li>
                   ))}
                 </ul>
               </div>
               <div>
-                <h1 className="uppercase text-xl sm:3xl font-semibold">
-                  Contact Details
-                </h1>
+                <h1 className="uppercase text-xl sm:3xl font-semibold">Contact Details</h1>
               </div>
             </div>
           ))}
         </div>
         <div className="sm:w-2/4 space-y-4">
           <div className="border p-4 rounded-xl">
-            <h1 className="font-semibold text-lg sm:text-2xl text-primary">
-              Amount Paid
-            </h1>
+            <h1 className="font-semibold text-lg sm:text-2xl text-primary">Amount Paid</h1>
             <p className="flex justify-between">
-              <span className="font-semibold">
-                {totalPassengers} passengers
-              </span>
-              <span className="text-primary font-semibold">
-                £ {bookingData[0].totalPrice}
-              </span>
+              <span className="font-semibold">{totalPassengers} passengers</span>
+              <span className="text-primary font-semibold">£ {bookingData[0].totalPrice}</span>
             </p>
             <p>includes taxes</p>
           </div>
           <div className="border p-4 rounded-xl">
-            <h1 className="font-semibold text-lg sm:text-2xl text-primary">
-              Airlines Contact
-            </h1>
+            <h1 className="font-semibold text-lg sm:text-2xl text-primary">Airlines Contact</h1>
             <p>
               <span>+07887 XXXXXXXXX</span>
             </p>
